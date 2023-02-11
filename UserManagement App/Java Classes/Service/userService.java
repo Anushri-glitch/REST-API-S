60b3a6cab9a69dd930aa7272bd0da33e98fc7924
Anushka Srivastava <47708011+Anushri-glitch@users.noreply.github.com>
2023-02-11 18:50:53 +0530

    Update userService.java

--- UserManagement App/Java Classes/Service/userService.java
+++ UserManagement App/Java Classes/Service/userService.java
@@ -1,67 +1,57 @@
-package com.example.userManage.model;
+package com.example.userManage.service;
 
-public class manage {
-    private int id;
-    private String name;
-    private String username;
-    private String address;
-    private String phoneNum;
+import com.example.userManage.model.manage;
+import org.springframework.stereotype.Service;
 
-    public manage(int id, String name, String username, String address, String phoneNum){
-        this.id = id;
-        this.name = name;
-        this.username = username;
-        this.address = address;
-        this.phoneNum = phoneNum;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Service
+public class userService {
+
+    private static List<manage> Manage = new ArrayList<>();
+
+    private static int manageCount =0;
+
+    static {
+        Manage.add(new manage(++manageCount,"Anushka","Anushka@1234","Allahabad,India","8081009534"));
+        Manage.add(new manage(++manageCount,"Prerna","prerna@365","Banglore,India","3478901245"));
+        Manage.add(new manage(++manageCount,"Mitali","mitali@pandit13","Delhi,India","9999334210"));
+        Manage.add(new manage(++manageCount,"Adhik","adhikMehta@444","Pune,India","5689347612"));
+        Manage.add(new manage(++manageCount,"Mayuresh","mayuresh@90","Goa,India","7056234100"));
+        Manage.add(new manage(++manageCount,"Nikhil","nikhil@45","Udaipur,India","6671235902"));
     }
 
-    public int getId() {
-        return id;
+    public List<manage> getAllUser() {
+        return Manage;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void addUser(manage addOne){
+        Manage.add(addOne);
     }
 
-    public String getName() {
-        return name;
+    public manage getUserById(int id){
+        Predicate<? super manage> predicate = todo -> todo.getId() == id;
+        manage manageId = Manage.stream().filter(predicate).findFirst().get();
+        return manageId;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void deleteUser(int id){
+        Predicate<? super manage> predicate = todo -> todo.getId() == id;
+        Manage.removeIf(predicate);
     }
 
-    public String getUsername() {
-        return username;
-    }
+    public void updateUserInfo(int id, manage newUser){
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+        //Step 1 : find todo to be Update
+        //Step 2 : update todo
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNum() {
-        return phoneNum;
-    }
-
-    public void setPhoneNum(String phoneNum) {
-        this.phoneNum = phoneNum;
-    }
-
-    public String toString(){
-        return "manage{" +
-                "id=" + id +
-                ",name='" + name + '\'' +
-                ",username='" + username + '\'' +
-                ",address='" + address + '\'' +
-                ",phoneNumber='" + phoneNum +
-                "}";
+        manage manageNew = getUserById(id);
+        manageNew.setId(newUser.getId());
+        manageNew.setName(newUser.getName());
+        manageNew.setUsername(newUser.getUsername());
+        manageNew.setAddress(newUser.getAddress());
+        manageNew.setPhoneNum(newUser.getPhoneNum());
     }
 }
